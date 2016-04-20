0da7566d22a70eeca9f9d1840ee7afe874c4bb40
MelanieJae <mjhanddev@gmail.com>
2016-04-20 08:50:23 -0400

    added if loops to handle user input regarding pattern type and needle-hook size

--- converter.rb
+++ converter.rb
@@ -7,21 +7,40 @@ knitnsizeUS = [1,2,3,4,5,6,8,9,10,10.5,11]
 # array of crochet hook sizes that have an equivalent knitting needle size
 crochethsize = ['B','C','D','E','F','G','H','I','J','K','L']
 
-puts knitnsizeUS[0]
-puts crochethsize[0]
+# a check for edits to size arrays
 puts knitnsizeUS.length == crochethsize.length
 
-print "Is the pattern a knitting pattern or a crochet pattern?"
+puts "Is the pattern a knitting pattern or a crochet pattern?"
 replypattern = gets.chomp
 
-
-if replypattern["knit"]
-	#this does not take gauge difference into account, assumes user is following pattern and gauge
-	#requirements exactly
+if replypattern ["knit"]
 	puts "What needle size does the pattern require?"
-	
 else
-	puts "What hook size does the pattern require"
+	puts "What size hook does the pattern require?"
 end
-
 replysize = gets.chomp
+
+knitnsizeUS.each do |i|
+	if replysize == knitnsizeUS[i]
+			# puts crochethsize[i]
+			#replysizeconvert = crochethsize[i]
+		puts "The equivalent crochet hook size is..." + "#{crochethsize[i]}"
+	end
+
+
+	# puts "What hook size does the pattern require?"	
+	# replysize = gets.chomp	
+# loops through knit needle size array to match with what the user put in and
+# returns the equivalent crochet hook size from the crochet hook size array or returns 
+# warning saying there is no equivalent crochet hook size and suggests either the next larger
+# hook or next smaller hook depending on gauge or how tight the person's knitting tends to be
+#, the fabric, etc...
+
+
+			# returns replysize to feed into the suggestion feature code below
+			# return replysize
+end
+# puts "Sorry, there is no equivalent crochet hook size. Hang on a sec for a couple more questions and a suggested hook size."
+
+# if replyconvert["knit"]
+# add suggestion feature here....
