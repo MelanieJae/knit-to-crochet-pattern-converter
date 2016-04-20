87e163ba58f048b05997a996c406bad397a88f1f
MelanieJae <mjhanddev@gmail.com>
2016-04-20 09:28:03 -0400

    added loops to go through needle and hook size arrays to match input and give back an equivalent hook/needle size

--- converter.rb
+++ converter.rb
@@ -7,7 +7,7 @@ knitnsizeUS = [1,2,3,4,5,6,8,9,10,10.5,11]
 # array of crochet hook sizes that have an equivalent knitting needle size
 crochethsize = ['B','C','D','E','F','G','H','I','J','K','L']
 
-# a check for edits to size arrays
+# a check for edits to size arrays above
 puts knitnsizeUS.length == crochethsize.length
 
 puts "Is the pattern a knitting pattern or a crochet pattern?"
@@ -15,18 +15,29 @@ replypattern = gets.chomp
 
 if replypattern ["knit"]
 	puts "What needle size does the pattern require?"
+	# gets.chomp reads needle sizes as strings so a conversion of the user's input is needed
+	# only when converting from knit size to crochet size
+	@replysizeknit = gets.chomp.to_i
 else
 	puts "What size hook does the pattern require?"
+	@replysizecrochet = gets.chomp
 end
-replysize = gets.chomp
 
-knitnsizeUS.each do |i|
-	if replysize == knitnsizeUS[i]
-			# puts crochethsize[i]
-			#replysizeconvert = crochethsize[i]
-		puts "The equivalent crochet hook size is..." + "#{crochethsize[i]}"
+knitnsizeUS.each_index do |k|
+	if @replysizeknit == knitnsizeUS[k]	
+		puts "The equivalent crochet hook size is: " + "#{crochethsize[i]}"
 	end
+end
 
+crochethsize.each_index do |c|
+	if @replysizecrochet == crochethsize[c]
+		puts "The equivalent knitting needle size is: " + "#{knitnsizeUS[c]}"
+	end
+end
+
+# for cases where there is no equivalent crochet hook size or needle size....
+# there's more likely to be no crochet equivalent for a knitting needle size so for efficiency
+# the 
 
 	# puts "What hook size does the pattern require?"	
 	# replysize = gets.chomp	
@@ -39,7 +50,7 @@ knitnsizeUS.each do |i|
 
 			# returns replysize to feed into the suggestion feature code below
 			# return replysize
-end
+
 # puts "Sorry, there is no equivalent crochet hook size. Hang on a sec for a couple more questions and a suggested hook size."
 
 # if replyconvert["knit"]
